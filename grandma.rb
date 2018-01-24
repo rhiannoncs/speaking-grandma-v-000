c5f47b978c14d7b82463c465861e25e55a456688
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-24 05:12:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     response = "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase
+  elsif phrase == phrase.upcase
     response = "NO, NOT SINCE 1938!"
   else
     response = "HUH?! SPEAK UP, SONNY!"
